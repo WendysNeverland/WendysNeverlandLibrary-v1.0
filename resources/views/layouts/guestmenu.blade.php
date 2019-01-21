cc367390379ae76a00a050a2828525fcdcfb0e44
Wendy <s_wen@msn.com>
2019-01-21 17:35:21 +0100

    begin menu guest

--- resources/views/layouts/guestmenu.blade.php
+++ resources/views/layouts/guestmenu.blade.php
@@ -1,16 +1,19 @@
-<div>
-    <ul class="menu align-right">
-        <li><a href="/admin/book">Book</li>
-        <li><a href="/admin/audiobook">Audiobook</a></li>
-    </ul>
-    <ul class="menu align-right">
-        <li>
-            <a href="{{ route('login') }}">{{ __('Login') }}</a>
-        </li>
-        <li>
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}">{{ __('Register') }}</a>
-            @endif
-        </li>
-    </ul>
-</div>
+    <div class="top-bar-left">
+        <ul class="menu">
+            <li><a href="/admin/book">Book</li>
+            <li><a href="/admin/audiobook">Audiobook</a></li>
+        </ul>
+    </div>
+    <div class="top-bar-right">
+        <ul class="menu">
+            <li>
+                <a href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            <li>
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">{{ __('Register') }}</a>
+                @endif
+            </li>
+        </ul>
+    </div>
+
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,19 +1,14 @@
-<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
-            {{-- hierboven komt uiteindelijk het logo --}}
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<div class="top-bar">
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            {{-- <a href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a> --}}
+
+            <div>
                 @guest
                     @include('layouts/guestmenu')
                 @else
                     @include('layouts/usermenu')
                 @endguest
-        </div>
-    </div>
-</nav>
+            </div>
+</div>
